19dc08bed9207837f3b5ee22ceccff807f5a1e40
whkessler <82409360+whkessler@users.noreply.github.com>
2021-05-06 17:54:11 -0500

    x

--- flask/understandingrouting/understanding.py
+++ flask/understandingrouting/understanding.py
@@ -1,9 +1,23 @@
-from flask import flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index ():
     return "Hello World!"
+@app.route('/dojo')
+def dojo():
+    return "Dojo!"
+@app.route('/say/<name>')
+def hi(name):
+    return f'Hi {name}!'
+@app.route('/repeat/<num>/<name>')
+def repeat(num,name):
+    result = name * int(num)
+    return f"{result}"
+
+
+
+
 
 
 
