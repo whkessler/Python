94eb94271c30dcf8427ccca78c3ebffa2ef5f90b
whkessler <82409360+whkessler@users.noreply.github.com>
2021-05-06 12:34:50 -0500

    new

--- flask/hello_flask/hello.py
+++ flask/hello_flask/hello.py
@@ -6,7 +6,10 @@ def hello_world():
 @app.route('/success')
 def success():
     return "success"
-
+@app.route('/hello/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+def hello(name):
+    print(name)
+    return "Hello, " + name
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
